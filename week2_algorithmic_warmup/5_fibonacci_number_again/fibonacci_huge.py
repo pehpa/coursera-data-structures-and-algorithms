0187ed636c71e84f679a408517e7f0a78be28421
Alexander Glantz <alexander.glantz@gmail.com>
2024-03-03 22:09:13 +0100

    cleanup, remove unused functions

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -18,25 +18,6 @@ def fibonacci_huge_naive(n, m):
     return current % m
 
 
-# def fib_fast_doubling(n):
-#     if n < 0:
-#         raise ValueError("Negative arguments not implemented")
-#     return _fib(n)[0]
-
-
-# def _fib(n):
-#     if n == 0:
-#         return (0, 1)
-#     else:
-#         a, b = _fib(n // 2)
-#         c = a * (b * 2 - a)
-#         d = a * a + b * b
-#         if n % 2 == 0:
-#             return (c, d)
-#         else:
-#             return (d, c + d)
-
-
 def pisano(m):
     prev = 0
     curr = 1
