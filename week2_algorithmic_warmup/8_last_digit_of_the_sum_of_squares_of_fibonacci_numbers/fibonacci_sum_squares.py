f387bfaf16e4f82749e3ec7b42f566cd52071da4
Alexander Glantz <alexander.glantz@gmail.com>
2024-03-03 23:19:23 +0100

    add solution for Last Digit of the Sum of Squares of Fibonacci Numbers

--- week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -1,3 +1,4 @@
+
 def fibonacci_sum_squares(n):
     if n <= 1:
         return n
@@ -11,6 +12,17 @@ def fibonacci_sum_squares(n):
     return sum % 10
 
 
+def fibonacci_sum_squares_fast(n):
+    # uses F0^2 + F1^2 + ... + Fn^2 = Fn + Fn+1
+    curr, next = 0, 1
+
+    for i in range(n % 60):
+        curr, next = next, (curr + next) % 10
+
+    return (curr * next) % 10
+
+
 if __name__ == '__main__':
     n = int(input())
-    print(fibonacci_sum_squares(n))
+    # print(fibonacci_sum_squares(n))
+    print(fibonacci_sum_squares_fast(n))
