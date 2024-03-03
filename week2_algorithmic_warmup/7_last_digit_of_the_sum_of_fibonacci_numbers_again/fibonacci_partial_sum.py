6641ad85fc6605d60a5ceec046de3b391e8961d0
Alexander Glantz <alexander.glantz@gmail.com>
2024-03-03 22:47:28 +0100

    add fast solution for Last Digit of the Partial Sum of Fibonacci Numbers

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -14,8 +14,31 @@ def fibonacci_partial_sum_naive(from_, to):
     return _sum % 10
 
 
+def fibonacci_partial_sum_fast(from_, to):
+    assert from_ <= to  # from the assumptions in the problem statement
+    
+    m = from_ % 60  # pisano_length(10) = 60
+    n = to % 60  # pisano_length(10) = 60
+
+    # if n is smaller than m, add one more pisano period to n
+    if n < m:
+        n += 60
+
+    curr, next, sum = 0, 1, 0
+
+    for i in range(n + 1):
+        if i >= m:
+            sum += curr
+        curr, next = next, next + curr
+
+    return sum % 10
+
+
 if __name__ == '__main__':
     # input = sys.stdin.read();
     # from_, to = map(int, input.split())
     from_, to = map(int, input().split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    # print(fibonacci_partial_sum_naive(from_, to))
+    # from_, to = 5618252, 6583591534156
+    # from_, to = 10, 10
+    print(fibonacci_partial_sum_fast(from_, to))
