13afe93a336fe8e4ef9e94a5123e395aa7dda341
Alexander Glantz <alexander.glantz@gmail.com>
2024-03-08 20:37:43 +0100

    add solution for greedy money change

--- course1-algorithmic-toolbox/week3_greedy_algorithms/1_money_change/change.py
+++ course1-algorithmic-toolbox/week3_greedy_algorithms/1_money_change/change.py
@@ -1,9 +1,19 @@
 def change(money):
-    # write your code here
+    num_coins = 0
+    while money > 0:
+        if money // 10 > 0:
+            max_coin = 10
+        elif money // 5 > 0:
+            max_coin = 5
+        else:
+            max_coin = 1
+        money = money - max_coin
+        num_coins = num_coins + 1
 
-    return money
+    return num_coins
 
 
 if __name__ == '__main__':
     m = int(input())
-    print(change(m))
+    num_coins = change(m)
+    print(num_coins)
