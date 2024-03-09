e3f9d7120f27f61c52e4a0d24a4316490da7a2e9
Alexander Glantz <alexander.glantz@gmail.com>
2024-03-09 11:19:46 +0100

    add greedy solution to car fuelling problem

--- course1-algorithmic-toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ course1-algorithmic-toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -1,11 +1,55 @@
 from sys import stdin
 
 
+def get_next_stop(max_reach, curr_mile, stops):
+    next_stop = -1
+    for stop in stops:
+        if stop > curr_mile and stop <= max_reach:
+            next_stop = stop
+    return next_stop
+
+
 def min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+    total_stops = 0
+    curr_mile = 0
+    max_reach = curr_mile + tank
+
+    while max_reach < distance:
+        # print('curr mile before = {:>4}, max reach before = {:>4}, '.format(curr_mile, max_reach), end='')
+
+        # get next stop distance and set current mile
+        curr_mile = get_next_stop(max_reach, curr_mile, stops)
+        # print('curr mile after = {:>4}'.format(curr_mile), end='')
+
+        # if no next stop could be found within reach, break and state that destination is not reachable
+        if curr_mile == -1:
+            total_stops = -1
+            # print(" (destination cannot be reached)")
+            break
+        # if next stop could be found, one more stop was done and current mile is stop mile
+        else:
+            total_stops = total_stops + 1
+            max_reach = curr_mile + tank
+            # print(', max reach after = {:>4}'.format(max_reach))
+
+    return total_stops
 
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, stdin.read().split())
+
+    # d = 950
+    # m = 400
+    # stops = [200, 375, 550, 750]
+
+    # d = 10
+    # m = 3
+    # stops = [1, 2, 5, 9]
+
+    # d = 200
+    # m = 250
+    # stops = [100, 150]
+
+    # print(d, m, stops)
+
     print(min_refills(d, m, stops))
