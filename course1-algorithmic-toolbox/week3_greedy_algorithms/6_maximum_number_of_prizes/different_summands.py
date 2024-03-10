36c46ac544e850c4f7431b25ac05d217bb1270bf
Alexander Glantz <alexander.glantz@gmail.com>
2024-03-10 12:36:27 +0100

    add fast solution for maximum number of prizes problem

--- course1-algorithmic-toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ course1-algorithmic-toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,11 +1,29 @@
 def optimal_summands(n):
     summands = []
-    # write your code here
+
+    m = 1
+    r = n - 1
+    
+    while m < r:
+        summands.append(m)
+        # print("while: m={}, summands={}, r={}".format(m, summands, r))
+        m = m + 1
+        r = r - m
+    summands.append(r + m)
+    # print("summands = {}, sum = {}".format(summands, sum(summands)))
+
     return summands
 
 
 if __name__ == '__main__':
     n = int(input())
+
+    # n = 6  # result: 3 / 1 2 3
+    # n = 8  # result: 3 / 1 2 5
+    # n = 2  # result: 1 / 2
+    # n = 454351
+
     summands = optimal_summands(n)
+
     print(len(summands))
     print(*summands)
